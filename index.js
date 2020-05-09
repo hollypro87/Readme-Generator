5fc8b3f585c121fae3b5d0a68a8b65385e942c60
Holly Prothe <hollyprothe1@gmail.com>
2020-05-09 09:30:36 -0500

    add questions and code to add new file

--- index.js
+++ index.js
@@ -1,12 +1,102 @@
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const md = require("generateMarkdown");
 
-];
+const writeFileAsync = util.promisify(fs.writeFile);
 
-function writeToFile(fileName, data) {
+function generateMarkdown(data) {
+    return `
+  # ${data.title}
+
+  # ${data.description}
+
+  # Table of Contents
+  * Installation
+  * Usage
+  * License
+  * Contributing
+  * Tests
+  * FAQ
+
+  # ${data.installation}
+
+  # ${data.usage}
+
+  # ${data.contributing}
+
+  # ${data.tests}
+
+  # ${data.questions}
+  
+  `;
+
 }
 
-function init() {
+module.exports = generateMarkdown;
 
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "username",
+            message: "What is your GitHub username?"
+        },
+        {
+            type: "input",
+            name: "title",
+            message: "What is your project title?"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "What is a brief description of your project?"
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "How do you install this program?"
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "How do you use this program?"
+        },
+        {
+            type: "input",
+            name: "license",
+            message: "Enter license information here."
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Enter who has contributed to this program."
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "How do you test if the program is working?"
+        },
+        {
+            type: "input",
+            name: "questions",
+            message: "Enter frequently asked questions here."
+        }
+    ]);
+}
+async function init() {
+    console.log("hi")
+    try {
+        const answers = await promptUser();
+
+        const md = generateMarkdown(answers);
+
+        await writeFileAsync("README.md", md);
+
+        console.log("Successfully wrote ReadMe file.");
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 init();
